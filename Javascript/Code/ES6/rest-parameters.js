b12e9fbd8efa103cf840341bc4ce5bf69f5bc151
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-11-10 11:54:21 +0600

    Updated

--- Javascript/Code/ES6/rest-parameters.js
+++ Javascript/Code/ES6/rest-parameters.js
@@ -1,4 +1,5 @@
 function sum(...args) {
+  // Here ... is represent as rest param meaning it copying all the arguments as array that are passed in the function
   console.log(args);
   let total = 0;
   for (const a of args) {
