b7e2e550288606353652f185347fa11d5f4b502e
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-11-10 11:48:03 +0600

    default param added

---
+++ Javascript/Code/ES6/default-parameters.js
@@ -0,0 +1,7 @@
+function defaultParamExample(name = "Shahin") {
+  // Here "Shahin" is default param
+  console.log(name);
+}
+
+defaultParamExample("Test"); // Here "Test" is arguments (What we pass in the function is called arguments)
+defaultParamExample();
