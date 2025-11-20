d21b08862d2d0ecd84432ac254a3f166f267ae9a
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-11-20 10:01:31 +0600

    added this

--- Javascript/Code/this.js
+++ Javascript/Code/this.js
@@ -1,3 +1,12 @@
 // The this keyword refers to the context where a piece of code, such as a function's body, is supposed to run.
 // Most typically, it is used in object methods, where this refers to the object that the method is attached to,
 // thus allowing the same method to be reused on different objects.
+
+const person = {
+  name: "John",
+  greet: function () {
+    return `Hello, my name is ${this.name}`;
+  },
+};
+
+console.log(person.greet()); // Output: Hello, my name is John
