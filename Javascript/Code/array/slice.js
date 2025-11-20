6423a5c1967c193bde619012fc0623d614a49c96
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-11-20 10:29:00 +0600

    added splice and slice method

---
+++ Javascript/Code/array/slice.js
@@ -0,0 +1,3 @@
+let array = [1, 2, 3, 4, 5];
+let slicedArray = array.slice(1, 4); // Start Index(inclusive), End Index(exclusive)
+console.log(slicedArray); // Output: [2, 3, 4]
---
+++ Javascript/Code/array/splice.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5];
+let splicedArray = array.splice(1, 3); // Start Index, Number of Elements to Remove
+console.log(splicedArray); // Output: [2, 3, 4]
+console.log(array); // Output: [1, 5]
